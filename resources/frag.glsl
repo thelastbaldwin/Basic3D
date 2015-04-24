f078d572b6834e5e5772b3e67b9e66fd6d39b8bf
Steve Minor <steve.e.minor@nordstrom.com>
2015-04-24 11:29:16 -0700

    quick snapshot before I break everything

--- resources/frag.glsl
+++ resources/frag.glsl
@@ -7,7 +7,7 @@ varying vec2 vUv;
 
 void main(){
     vec4 texColor = texture2D(tex0, vUv);
-    if(gl_FragColor.rgb = Vec3(0, 0, 0)){
+    if(texColor.rgb == vec3(0, 0, 0)){
         discard;
     }else{
         gl_FragColor = vec4(texColor.rgb, opacity);
--- src/Basic3DApp.cpp
+++ src/Basic3DApp.cpp
@@ -25,18 +25,22 @@ class Basic3DApp : public AppNative {
     float largeCubeSize;
     float radius;
     float opacity;
+    int canvasOffset;
+    int frameOffset;
+    int currentFrame;
     
     CameraPersp	mCam;
-    const static int NUM_TEXTURES = 10;
+    const static int NUM_TEXTURES = 20;
     deque<ci::gl::Texture> mFrames;
     gl::GlslProgRef mShader;
   public:
 	void setup();
     void prepareSettings(Settings* settings);
 	void mouseDown( MouseEvent event );
-    void resize();
 	void update();
 	void draw();
+    void bindAllTextures(deque<ci::gl::Texture>& textures);
+    void unbindAllTextures(deque<ci::gl::Texture>& textures);
 };
 
 void Basic3DApp::prepareSettings(cinder::app::AppBasic::Settings *settings){
@@ -47,6 +51,8 @@ void Basic3DApp::prepareSettings(cinder::app::AppBasic::Settings *settings){
 
 void Basic3DApp::setup()
 {
+    currentFrame = 0;
+    
     gl::enableAlphaBlending();
     gl::enableDepthRead();
     gl::enableDepthWrite();
@@ -72,15 +78,19 @@ void Basic3DApp::setup()
     smallCubeSize = 20.0;
     largeCubeSize = 80.0;
     numCubes = 10;
-    radius = 400;
+    radius = 384;
     opacity = 0.31;
+    canvasOffset = 100;
+    frameOffset = 20;
     
-    mParams = params::InterfaceGl(getWindow(), "Parameters", Vec2i(200,120));
+    mParams = params::InterfaceGl(getWindow(), "Parameters", Vec2i(200,140));
     mParams.addParam( "Small Cube Size", &smallCubeSize, "min=0.1 max=20.5 step=0.5" );
     mParams.addParam( "Large Cube Size", &largeCubeSize, "min=10.0 max=100.0 step=0.5" );
     mParams.addParam( "numCubes", &numCubes, "min=0 max=10" );
     mParams.addParam( "radius", &radius, "min=100 max=800");
     mParams.addParam("opacity", &opacity, "min=0 max=1.0 step=0.05");
+    mParams.addParam("offset", &canvasOffset, "min=10 max=500 step=5");
+    mParams.addParam("frameOffset", &frameOffset, "min=1 max=20 step=1");
     
     //shader setup
     try {
@@ -95,17 +105,34 @@ void Basic3DApp::setup()
     }
     
     //texture deque setup
-    mFrames.resize(NUM_TEXTURES);
+    mFrames.resize(NUM_TEXTURES);   
+    for(int i = 0; i < NUM_TEXTURES; ++i){
+        mFrames[i] = ci::gl::Texture(getWindowWidth(), getWindowHeight());
+    }
 }
 
 void Basic3DApp::mouseDown( MouseEvent event )
 {
 }
 
-void Basic3DApp::resize(){
-    //subtle memory leak in here
-    mFbo.reset();
-    mFbo = gl::Fbo(getWindowWidth(), getWindowHeight());
+void Basic3DApp::bindAllTextures(deque<ci::gl::Texture> &textures){
+    stringstream ss;
+    string texLoc;
+    
+    for(int i = 0; i < textures.size(); ++i){
+        textures[i].enableAndBind();
+        ss << "tex" << i;
+        texLoc = ss.str();
+        mShader->uniform(texLoc, i);
+        ss.clear();
+        ss.str("");
+    }
+}
+
+void Basic3DApp::unbindAllTextures(deque<ci::gl::Texture> &textures){
+    for(auto tex: textures){
+        tex.unbind();
+    }
 }
 
 void Basic3DApp::update()
@@ -121,7 +148,6 @@ void Basic3DApp::draw()
     mFbo.bindFramebuffer();
     gl::setViewport( mFbo.getBounds());
     gl::clear(ColorAf(Color::black(), 0.0));
-//    gl::enableWireframe();
     gl::pushModelView();
     gl::translate(Vec3f(mFbo.getWidth()/2, mFbo.getHeight()/2, 0));
 
@@ -141,30 +167,43 @@ void Basic3DApp::draw()
     mFbo.unbindFramebuffer();
     
     //manage stack of textures
-//    mFrames.push_front(mFbo.getTexture());
-//    mFrames.pop_back();
+    if(currentFrame % frameOffset == 0){
+        mFrames.push_front(gl::Texture(mFbo.getTexture()));
+        mFrames.pop_back();
+    }
+    ++currentFrame;
     
     gl::setViewport( getWindowBounds());
     
-    mFbo.getTexture().enableAndBind();
-
-    if(mShader){
-        mShader->bind();
-        mShader->uniform("tex0", 0);
-        mShader->uniform("opacity", opacity);
-    }
+   
+//    mFrames[0].bind();
+//     mFbo.getTexture().enableAndBind();
+    
+    
     gl::pushModelView();
     gl::translate(getWindowWidth()/2 - mFbo.getWidth()/2, getWindowHeight()/2 - mFbo.getHeight()/2);
-    for(int i = NUM_TEXTURES; i >= 0  ; --i){
+    for(int i = NUM_TEXTURES-1; i >= 0; --i){
+        if(mShader){
+            mShader->bind();
+            mFrames[i].enableAndBind();
+            mShader->uniform("tex0", 0);
+            mShader->uniform("opacity", opacity);
+        }
         gl::pushModelView();
-        gl::translate(Vec3f(0, 0, i * -100));
+        gl::translate(Vec3f(0, 0, i * -canvasOffset));
         gl::drawSolidRect(Rectf(Vec2f(0, 0), Vec2f(mFbo.getWidth(), mFbo.getHeight())));
         gl::popModelView();
+        if(mShader){
+            mFrames[i].unbind();
+            mShader->unbind();
+        }
     }
     
     gl::popModelView();
-    mFbo.getTexture().unbind();
-    mShader->unbind();
+//    mFbo.getTexture().unbind();
+//    mFrames[0].unbind();
+//    unbindAllTextures(mFrames);
+//    mShader->unbind();
     
     mParams.draw();
 }
